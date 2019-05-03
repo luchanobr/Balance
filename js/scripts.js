ad810fad5f88e8422d7b49d4cc06dc3e1f1bf2d6
unknown <luciano.rosmarin@gmail.com>
2019-05-03 10:56:59 -0300

    cambio en el heeader del index

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
 $(document).ready(function(){
-     $('.owl-carousel').owlCarousel({
-        center: false,
-        items:5,
-        loop:true,
-        margin: 20,
-        nav:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            576:{
-                items:2,
-                nav: true
-            },
-            1000:{
-                items:4,
-                nav: true,
+
+        $('.owl-carousel').owlCarousel({
+            center: false,
+            items:5,
+            loop:true,
+            margin: 20,
+            nav:true,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                576:{
+                    items:2,
+                    nav: true
+                },
+                1000:{
+                    items:4,
+                    nav: true,
+                }
             }
-        }
-    });
-  });
+        });
+
+        $('a[href*="#"]').on('click', function(e) {
+            e.preventDefault()
+          
+            $('html, body').animate(
+              {
+                scrollTop: $($(this).attr('href')).offset().top,
+              },
+              500,
+              'linear'
+            )
+          })
+       
+});
+
+
