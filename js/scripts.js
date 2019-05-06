ab3ecdd7d25677d3cf748890c429a82ca15993d7
luchanobr <luciano.rosamrin@gmail.com>
2019-05-06 17:31:29 -0300

    termiando el blog y comenzo de las 3 razones de balance en el index

--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,32 @@ $(document).ready(function(){
             )
           })
        
+        $("#blogButton").on("mouseover", function (){
+            $("#arrow").addClass("arrow-blog");
+        } )
+        
+        $("#blogButton").on("mouseleave", function (){
+            $("#arrow").removeClass("arrow-blog");
+        } )
+
+        var data = {
+   
+            labels: ['Lun', 'Mar', 'Mier', 'Jue', 'Vie'],
+          
+            series: [
+              [750, 220, 450, 1223, 980]
+            ]
+          };
+         
+          var opciones ={
+            width: 300,
+            height: 200
+          };
+
+          new Chartist.Line('.ct-chart', data, opciones);
+        
+        
+
 });
 
 
