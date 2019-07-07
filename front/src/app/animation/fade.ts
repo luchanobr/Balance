1fc50547628b289b2a488e29bcc19ce517fb1f36
unknown <luciano.rosmarin@gmail.com>
2019-07-07 17:03:04 -0300

    mejoras y debug para pantallas small y medium

--- front/src/app/animation/fade.ts
+++ front/src/app/animation/fade.ts
@@ -11,6 +11,8 @@ export const fadeInOut=
           position: 'relative',
           top:0,
           left:0,
+          right:0,
+          bottom: 0,
           width: '100%',
           
         })
